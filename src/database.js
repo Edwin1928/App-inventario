ccc7e331c9d94acaf7343c2ca2dac644c282b417
Edwin1928 <44735615+Edwin1928@users.noreply.github.com>
2021-08-10 16:23:58 -0600

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 //mongodb://productos-app:c52eafa0411c513f42af51d98cb20d99@dokku-mongo-productos-app:27017/productos_app
-mongoose.connect('mongodb://127.0.0.1:27017/productos_app?authSource=productos_app&gssapiServiceName=mongodb', {
+mongoose.connect('mongodb://productos-app:c52eafa0411c513f42af51d98cb20d99@dokku-mongo-productos-app:27017/productos_app', {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false
