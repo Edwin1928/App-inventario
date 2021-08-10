21bd00aaaa8d213b8f3ef675b9a40884f9511705
Edwin1928 <44735615+Edwin1928@users.noreply.github.com>
2021-08-10 17:09:26 -0600

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 //mongodb://productos-app:c52eafa0411c513f42af51d98cb20d99@dokku-mongo-productos-app:27017/productos_app
-mongoose.connect('mongodb://productos-app:c52eafa0411c513f42af51d98cb20d99@dokku-mongo-productos-app:27017/productos_app', {
+mongoose.connect('mongodb://productos-app:c52eafa0411c513f42af51d98cb20d99@dokku-mongo-productos-app:20017/productos_app', {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false
